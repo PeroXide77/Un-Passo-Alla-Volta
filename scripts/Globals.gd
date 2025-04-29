04ef3b902be75bf6f92b13ab077dea527e47621a
PeroXide77 <dario.ungaro003@gmail.com>
2025-04-29 22:54:22 +0200

    qualche fixes e improvements

---
+++ scripts/Globals.gd
@@ -0,0 +1,61 @@
+extends Node
+##QUESTA è UN AUTOLOAD (O SINGLETON)
+## script che viene caricato automaticamente all'avvio del gioco e rimane attivo per tutte le scene.
+##Per tenere dati condivisi come punteggio, l'audio
+
+
+const loading_screen = "res://scenes/loading_page.tscn"
+var current_scene = null
+var next_scene = null
+var volume = 100
+var resolution = Vector2(1300,750)
+
+func _ready():
+	var root = get_tree().root
+	# Using a negative index counts from the end, so this gets the last child node of `root`.
+	current_scene = root.get_child(-1)
+	
+
+func _apply_global_size(node):
+	DisplayServer.window_set_size(resolution)
+	DisplayServer.window_set_position(Vector2(300,200))
+	if node is Control:
+		node.set_deferred("size", resolution)
+	for child in node.get_children():
+		if child is ProgressBar:
+			child.set_deferred("size", Vector2(Globals.resolution[0]/1.5, Globals.resolution[1]/8))
+			child.set_anchor_and_offset((SIDE_LEFT), 0.5, -Globals.resolution[0]/3)
+		else: 
+			if child is BaseButton: 
+				child.set_deferred("size", Vector2(Globals.resolution[0]/5, Globals.resolution[1]/2.5))
+			else: 
+				_apply_global_size(child)
+
+func goto_scene(path):
+	# This function will usually be called from a signal callback,
+	# or some other function in the current scene.
+	# Deleting the current scene at this point is
+	# a bad idea, because it may still be executing code.
+	# This will result in a crash or unexpected behavior.
+
+	# The solution is to defer the load to a later time, when
+	# we can be sure that no code from the current scene is running:
+
+	_deferred_goto_scene.call_deferred(path)
+
+
+func _deferred_goto_scene(path):
+	# It is now safe to remove the current scene.
+	current_scene.free()
+
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+
+	# Instance the new scene.
+	current_scene = s.instantiate()
+
+	# Add it to the active scene, as child of root.
+	get_tree().root.add_child(current_scene)
+
+	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
+	get_tree().current_scene = current_scene
--- scripts/loading_scene.gd
+++ scripts/loading_scene.gd
@@ -1,10 +1,8 @@
 extends Control
 
-var next_scene = coseDaRicordare.next_scene
+var next_scene = Globals.next_scene
 func _ready():
-	coseDaRicordare._apply_global_size(get_tree().root)
-	$ProgressBar.set_deferred("size", Vector2(coseDaRicordare.size[0]/1.5, coseDaRicordare.size[1]/8))
-	$ProgressBar.set_anchor_and_offset((SIDE_LEFT), 0.5, -coseDaRicordare.size[0]/3)
+	Globals._apply_global_size(get_tree().root)
 	ResourceLoader.load_threaded_request(next_scene)
 # in maniera asincrona salviamo la prossima scena in background
 
@@ -14,5 +12,6 @@ func _process(_delta): #delta è il tempo trascorso durante un frame
 	ResourceLoader.load_threaded_get_status(next_scene, progress) #proprio in numero, tipo 10%
 	$ProgressBar.value = progress[0]*100  #per la percentuale
 	if progress[0] == 1:
-		var packed_scene = ResourceLoader.load_threaded_get(next_scene)
-		get_tree().change_scene_to_packed(packed_scene);
+		$ProgressBar.value = progress[0]*100
+		await get_tree().create_timer(1.0).timeout
+		Globals.goto_scene(next_scene)
