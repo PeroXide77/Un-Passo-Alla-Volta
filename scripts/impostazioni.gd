9d6581ed9c359f9c25cd62edfced5320c2ded4f7
PeroXide77 <dario.ungaro003@gmail.com>
2025-05-07 13:24:42 +0200

    ho anche aggiunto il fullscreen

--- scripts/impostazioni.gd
+++ scripts/impostazioni.gd
@@ -2,6 +2,7 @@ extends Popup
 
 @onready var volume = $SfondoTrasparent/BoxImpostazioni/VBoxContainer/volume
 @onready var resolutionMenu = $SfondoTrasparent/BoxImpostazioni/VBoxContainer/OptionButton
+@onready var fullScreen = $SfondoTrasparent/BoxImpostazioni/VBoxContainer/CheckBox
 
 func _ready(): ##appena si apre il container
 	volume.value = Globals.volume;
@@ -22,3 +23,9 @@ func _on_option_button_item_selected(index: int) -> void:
 	var key = resolutionMenu.get_item_text(index)
 	DisplayServer.window_set_size(Globals.resolutions[key])
 	DisplayServer.window_set_position(Vector2(30,30))
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if(toggled_on):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
