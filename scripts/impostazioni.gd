4e20d95353dff9390d7f180b43e20f3702bffd13
PeroXide77 <dario.ungaro003@gmail.com>
2025-05-08 10:35:06 +0200

    ulteriore piccola modifica

--- scripts/impostazioni.gd
+++ scripts/impostazioni.gd
@@ -11,6 +11,8 @@ func _ready():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		resolutionMenu.set_disabled(true)
 		checkFS.set_visible(true)
+		fullScreen.set_pressed_no_signal(true)
+		
 	
 func _on_volume_value_changed(value: float) -> void:
 	Globals.volume = value;
@@ -27,7 +29,7 @@ func _on_option_button_item_selected(index: int) -> void:
 	DisplayServer.window_set_size(Globals.resolutions[key])
 	DisplayServer.window_set_position(Vector2(30,30))
 
-func _on_impostazioni_toggled(toggled_on: bool) -> void:
+func _on_full_screen_toggled(toggled_on: bool) -> void:
 	if(toggled_on):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		resolutionMenu.set_disabled(true)
