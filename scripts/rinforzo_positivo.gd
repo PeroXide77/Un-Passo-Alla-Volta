3fea7acb9fc109f5b31540830f2f6e4faa9b12c3
PeroXide77 <dario.ungaro003@gmail.com>
2025-05-13 16:55:40 +0200

    implementate nuove cose

--- scripts/rinforzo_positivo.gd
+++ scripts/rinforzo_positivo.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var postIts = get_tree().get_nodes_in_group("postIt")
+@onready var circles = get_tree().get_nodes_in_group("circles")
 
 func _ready() -> void:
 	show_postIts()
@@ -13,3 +14,8 @@ func show_postIts() -> void:
 	for postIt in postIts:
 		if postIt.get_meta("lv") < gs:
 			postIt.set_visible(true)
+	for circle in circles:
+		if circle.get_meta("lv") == Chatbot.get_currentLevel():
+			circle.set_visible(true)
+		else:
+			circle.set_visible(false)
