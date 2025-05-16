4ee8a6703fcfd245d1f119ff1a377e6be25a8f6c
PeroXide77 <dario.ungaro003@gmail.com>
2025-05-16 10:46:32 +0200

    fixate ulteriormente le impostazioni

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,14 +4,12 @@ extends Control
 @onready var checkImp = $VBoxContainer/Impostazioni/CheckImp
 @onready var checkExit = $VBoxContainer/Exit/CheckExit
 @onready var impPopUp = $Impostazioni
-@onready var lvSelect = $VBoxContainer/SelezioneLivelli
-@onready var imp = $VBoxContainer/Impostazioni
-@onready var exit = $VBoxContainer/Exit
+@onready var options = get_tree().get_nodes_in_group("options")
 
 func _process(_delta: float) -> void:
-	Globals.btn_hover(lvSelect)
-	Globals.btn_hover(imp)
-	Globals.btn_hover(exit)
+	for option in options:
+		if option is Button:
+			Globals.btn_hover(option)
 
 func _on_selezione_livelli_pressed() -> void:
 	checkLvl.set_visible(true)
@@ -27,6 +25,14 @@ func _on_impostazioni_pressed() -> void:
 	checkImp.set_visible(true)
 	await get_tree().create_timer(0.5).timeout
 	impPopUp.popup()
+	for option in options:
+		if option is Button:
+			option.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
+			option.set_disabled(true)
 
 func _on_impostazioni_popup_hide() -> void:
+	for option in options:
+		if option is Button:
+			option.set_mouse_filter(Control.MOUSE_FILTER_STOP)
+			option.set_disabled(false)
 	checkImp.set_visible(false)
